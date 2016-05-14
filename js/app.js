b87de6baf8186d40fabc24309b64bd71af620443
Ebony <Ebony@Ebonys-MacBook-Pro.local>
2016-05-13 19:35:38 -0700

    update

--- js/app.js
+++ js/app.js
@@ -2,13 +2,6 @@ $(document).ready(function(){
 
 
 $(function() {
-      //----- OPEN
-      // $('[.popup]').on('onload', function(e)  {
-          // var targeted_popup_class = jQuery(this).attr('data-popup-1');
-          // $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
-
-          // event.preventDefault();
-      // });
 
       //----- CLOSE
       $('[data-popup-close]').on('click', function(e)  {
@@ -17,30 +10,13 @@ $(function() {
 
           event.preventDefault();
       });
-  // });
 
-
-  // window.onload = function () {
-  //     setTimeout(function () {
-  //         PopUp('show');
-  //     }, 5000);
-  // }
 
   window.setTimeout(function(){
     $('.popup').css({"display": "block"})
   }, 3000);
 });
 
-  // $(".subscribe-me").subscribeBetter();
-  // });
-
-
-  // alert("BE THE FIRST TO KNOW WHEN INTRODUCTION ARRIVE")
-  // $("h1").click(
-    // function(){
-      // alert("You clicked the headline")
-    // }
-  // )
 
 
 });
